a98640eb9b1c0349b404f601b94427fedf1dbcf8
tang dixi <dixi.tang@bindo.com>
2018-04-02 01:14:48 +0800

    fuck

--- FaceScan/FaceScan/ViewController.m
+++ FaceScan/FaceScan/ViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "GPUImageBeautifyFilter.h"
 
 @interface ViewController () <ARSCNViewDelegate, ARSessionDelegate>
 
 @property (nonatomic, strong) ARSCNView *sceneView;
 @property (nonatomic, strong) SCNNode *sceneNode;
 @property (nonatomic, strong) SCNNode *faceNode;
+
+@property (nonatomic, strong) GPUImageView *captureView;
+@property (nonatomic, strong) GPUImageVideoCamera *camera;
+@property (nonatomic, strong) GPUImageBeautifyFilter *beautifyFilter;
 
 @property (nonatomic, strong) dispatch_queue_t faceTrackingQueue;
 
@@ -25,12 +30,19 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     [self setupViews];
+	[self setupLive];
     [self setupSession];
 }
 
 - (void)setupViews {
-    
-    [self.view addSubview:self.sceneView];
+	[self.view addSubview:self.captureView];
+	[self.view addSubview:self.sceneView];
+}
+
+- (void)setupLive {
+	[self.camera addTarget:self.beautifyFilter];
+	[self.beautifyFilter addTarget:self.captureView];
+	[self.camera startCameraCapture];
 }
 
 - (void)setupSession {
@@ -39,12 +51,11 @@
     configuration.lightEstimationEnabled = YES;
     
     [self.sceneView.session runWithConfiguration:configuration
-                                         options:ARSessionRunOptionResetTracking |ARSessionRunOptionRemoveExistingAnchors];
+                                         options:ARSessionRunOptionResetTracking | ARSessionRunOptionRemoveExistingAnchors];
 }
 
 - (void)updateFaceNode {
     [self.sceneNode.childNodes makeObjectsPerformSelector:@selector(removeFromParentNode)];
-    
     [self.sceneNode addChildNode:self.faceNode];
 }
 
@@ -70,47 +81,25 @@
 #pragma mark - ARSessionDelegate
 
 - (void)session:(ARSession *)session didUpdateFrame:(ARFrame *)frame {
-    
-    CIImage *ciImage = [CIImage imageWithCVPixelBuffer:frame.capturedImage];
-    
-    CGAffineTransform translate = CGAffineTransformMakeTranslation(ciImage.extent.size.height, 0);
-    CGAffineTransform scale = CGAffineTransformMakeScale(0.821, 1);
-    CGAffineTransform rotate = CGAffineTransformMakeRotation(M_PI/2);
-    
-    CGAffineTransform transform = CGAffineTransformConcat(rotate, translate);
-    
-    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-
-    CGAffineTransform translation2 = [frame displayTransformForOrientation:orientation viewportSize:self.view.bounds.size];
-    
-    CIImage *rotateImage = [ciImage imageByApplyingTransform:translation2];
-    CGAffineTransform translate2 = CGAffineTransformMakeTranslation(rotateImage.extent.size.width, 0);
-    
-    CIImage *finalImage = [rotateImage imageByApplyingTransform:translate];
-    
-    CIFilter *filter = [CIFilter filterWithName:@"CIColorInvert"];
-    [filter setValue:nil
-              forKey:kCIInputImageKey];
-    
-    CIContext *context = [[CIContext alloc] init];
-    
-    CGImageRef cgImage = [context createCGImage:filter.outputImage
-                                       fromRect:CGRectMake(0, 0, rotateImage.extent.size.width, rotateImage.extent.size.height)];
-    
-    
-//    self.sceneView.scene.background.contents = (__bridge id)cgImage;
-    
-//    CGImageRelease(cgImage);
+	
+	
+	
 }
 
 #pragma mark - Lazy Loading
 
 - (ARSCNView *)sceneView {
     if (! _sceneView) {
-        _sceneView = [[ARSCNView alloc] initWithFrame:self.view.bounds options:nil];
+			
+		CGRect rect = CGRectMake(0, 0, 375, 812);
+			
+        _sceneView = [[ARSCNView alloc] initWithFrame:rect options:nil];
+		_sceneView.center = self.view.center;
+		
         _sceneView.showsStatistics = YES;
         _sceneView.delegate = self;
         _sceneView.session.delegate = self;
+		
     }
     return _sceneView;
 }
@@ -118,8 +107,8 @@
 - (SCNNode *)faceNode {
     if (! _faceNode) {
         ARSCNFaceGeometry *faceGeometry = [ARSCNFaceGeometry faceGeometryWithDevice:self.sceneView.device];
-        faceGeometry.firstMaterial.diffuse.contents = UIColor.clearColor;
-        faceGeometry.firstMaterial.lightingModelName = SCNLightingModelPhysicallyBased;
+		faceGeometry.firstMaterial.diffuse.contents = UIColor.whiteColor;
+        faceGeometry.firstMaterial.lightingModelName = SCNLightingModelPhong;
         
         _faceNode = [[SCNNode alloc] init];
         _faceNode.geometry = faceGeometry;
@@ -132,6 +121,29 @@
         _faceTrackingQueue = dispatch_queue_create("com.sceneKit.faceTracking.whee", DISPATCH_QUEUE_CONCURRENT);
     }
     return _faceTrackingQueue;
+}
+
+- (GPUImageView *)captureView {
+	if (! _captureView) {
+		_captureView = [[GPUImageView alloc] initWithFrame:self.sceneView.frame];
+	}
+	return _captureView;
+}
+
+- (GPUImageVideoCamera *)camera {
+	if (! _camera) {
+		_camera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
+		_camera.horizontallyMirrorFrontFacingCamera = YES;
+		_camera.outputImageOrientation = UIInterfaceOrientationPortrait;
+	}
+	return _camera;
+}
+
+- (GPUImageBeautifyFilter *)beautifyFilter {
+	if (! _beautifyFilter) {
+		_beautifyFilter = [[GPUImageBeautifyFilter alloc] init];
+	}
+	return _beautifyFilter;
 }
 
 @end
